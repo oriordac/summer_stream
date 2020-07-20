150e9390d50d64e4f64a9d1f8e570e853a5fff5f
Mike Parish <michaeljparish@gmail.com>
2020-07-20 18:36:50 -0400

    Added dependencies and setup fetch

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,45 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+
+    <div class="field">
+      <label class="label">Name</label>
+      <div class="control">
+        <input class="input" type="text" placeholder="Text input" v-model="query">
+      </div>
+    </div>
+
+    <div class="control">
+      <button class="button is-link" @click="fetchAPI">Submit</button>
+    </div>
+
+    <div v-if="rowdata">
+      <h3>{{rowdata.Data}}</h3>
+    </div>
+
   </div>
 </template>
+
+<script>
+
+import axios from 'axios'
+
+export default {
+  data: () => ({
+    query: "",
+    rowdata: {},
+    url_base: "https://vzafhox7zf.execute-api.us-east-2.amazonaws.com/dev/se3/"
+  }),
+  methods: {
+    fetchAPI () {
+      axios.get(`${this.url_base}?name=${this.query}`)
+        .then(res => {
+          return res.data;
+      }).then(this.setResults);
+    },
+    setResults (results) {
+      this.rowdata = results;
+    },
+  }
+}
+</script>
